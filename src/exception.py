f3832ed9c4c0df0cf0e34c80c10e757776d00ba4
ncheymbamalu <nchey.learnings@gmail.com>
2023-04-18 11:11:05 -0700

    created project structure, specified logging and exception handling

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys
+import logging
+
+from src import logger
+
+
+def return_error_message(error, detail: sys) -> str:
+    _, _, execution_info = detail.exc_info()
+    filename: str = execution_info.tb_frame.f_code.co_filename
+    line_number: int = execution_info.tb_lineno
+    message = f"Error occurred in [{filename}] on line [{line_number}]; Error message: '{str(error)}'"
+    return message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message: str = return_error_message(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+from datetime import datetime
+
+FILENAME = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+FILEPATH = os.path.join(os.getcwd(), "logs")
+os.makedirs(FILEPATH, exist_ok=True)
+
+logging.basicConfig(
+    filename=os.path.join(FILEPATH, FILENAME),
+    format="[%(asctime)s] - %(pathname)s - %(lineno)d - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
